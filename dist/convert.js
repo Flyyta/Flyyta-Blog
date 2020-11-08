b1e749b2faa6e36afa33bee5e0c25056c7eb2d52
Abhishek Mogaveera <37222497+Abhishek971999@users.noreply.github.com>
2020-11-08 19:12:38 +0530

    update

--- dist/convert.js
+++ dist/convert.js
@@ -2,28 +2,11 @@ const fs = require("fs");
 const util = require("util");
 const config = require("./config");
 const readFile = util.promisify(fs.readFile);
+const postMapFunction = require("./layout/index-page-posts");
 
 const regex = /\{(.*?)\}/g;
 const moveFrom = config.filePath.postsdir;
 const moveTo = config.filePath.outdir;
-
-const postMapFunction = (posts) => {
-  return `
-          <div class="posts">
-              ${posts
-                .map(
-                  (post) => `<div class="post">
-                  <h3><a href="./${post.path}">${post.attributes.title}</a></h3>
-                      <small>${new Date(
-                        parseInt(post.attributes.date)
-                      ).toDateString()}</small>
-                    <p>${post.attributes.description}</p>
-                  </div>`
-                )
-                .join("")}
-          </div>
-        `;
-};
 
 //Function to read file asynchronously
 const readFileAsync = async (filename) => {
--- dist/posts.js
+++ dist/posts.js
@@ -2,45 +2,7 @@ const config = require("./config");
 const fm = require("front-matter");
 const fs = require("fs");
 const marked = require("./marked");
-
-//Single Blog Post Template
-const posthtml = (data) => `
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="${data.attributes.description}" />
-        <link rel="stylesheet" href="/assets/styles/grotesk.light.css">
-        <link rel="stylesheet" href="/assets/styles/highlights.css">
-        <link rel="stylesheet" href="/assets/styles/main.css">
-        <title>${data.attributes.title}</title>
-    </head>
-    <body>
-        <div class="grotesk">
-            <header>
-                <a href="/">Go back home</a>
-                <p>—</p>
-            </header>
-
-            <div class="content">
-                <h1>${data.attributes.title}</h1>
-                <p>${new Date(
-                  parseInt(data.attributes.date)
-                ).toDateString()}</p>
-                <hr />
-                ${data.body}
-            </div>
-
-            <footer>
-                ${`<p>© ${new Date().getFullYear()} ${
-                  config.authorName
-                }, Find the code on <a href="github.com/kartiknair/blog">GitHub</a></p>`}
-            </footer>
-        </div>
-    </body>
-</html>
-`;
+const singleBlogTemplate = require("./layout/blog-post-layout");
 
 //Read and process markdown file
 const createPost = (postPath) => {
@@ -62,7 +24,7 @@ const createPosts = (posts) => {
 
     fs.writeFile(
       `${config.postPath.outdir}/${post.path}/index.html`,
-      posthtml(post),
+      singleBlogTemplate(post),
       (e) => {
         if (e) throw e;
         console.log(`${post.path}/index.html was created successfully`);
