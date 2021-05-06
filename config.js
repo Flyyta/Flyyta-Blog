6d259dc62b2bf438a9f19bd42ce8575f298be854
Abhishek Mogaveera <37222497+Abhishek971999@users.noreply.github.com>
2021-05-06 20:59:10 +0530

    update

--- config.js
+++ config.js
@@ -1,19 +1,29 @@
 // The file config.js defines your site’s metadata and other general configuration.
 
 const config = {
-    siteName: "Flyyta Blog",
-    siteHeading: "Website created using Flyyta",
-    siteAuthorName: "Flyyta",
-    siteAuthorWebsite: "https://flyyta.netlify.app",
-    postPath: {
-      postsdir: "./content",
-      outdir: "./public",
-    },
-    filePath: {
-      postsdir: "./src",
-      outdir: "./public",
-    },
-  };
-  
-  module.exports = config;
-  
+  siteName: "Flyyta Blog",
+  siteHeading: "Website created using Flyyta",
+  siteAuthorName: "Flyyta",
+  siteAuthorWebsite: "https://flyyta.netlify.app",
+  postPath: {
+    postsdir: "./content",
+    outdir: "./public",
+  },
+  filePath: {
+    postsdir: "./src",
+    outdir: "./public",
+  },
+  mapPostsTo: {
+    //todo: add directory then filename ..
+    fileName: "index.html",
+    filedir: "./src",
+  },
+  postsLayout: {
+    postsdir: "./layout/singleBlog",
+  },
+  blogListLayout: {
+    postsdir: "./layout/singleBlog",
+  },
+};
+
+module.exports = config;
